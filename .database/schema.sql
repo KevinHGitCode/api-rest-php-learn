9591a97bebbdb165faec558db6d1659bf731da29
KevinHGitCode <kevinhafiddiazgarcia2003@gmail.com>
2024-12-30 05:34:37 -0500

    Se creo la estructura del proyecto

---
+++ .database/schema.sql
@@ -0,0 +1,1 @@
+/* TODO: Create the schema for the database with test data */
---
+++ model/UserModel.php
@@ -0,0 +1,7 @@
+<?php
+// TODO: Implement the UserModel class
+
+class UserModel
+{
+    public function __construct() {}
+}
